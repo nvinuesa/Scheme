e18c08e0034674c376c3ad4c28a987965fd37426
underscorenico <vinuesa.nico@gmail.com>
2018-03-14 22:58:25 +0100

    Added equals methods

--- src/Scheme/Evaluator.hs
+++ src/Scheme/Evaluator.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Scheme.Evaluator
   ( eval
   ) where
@@ -57,6 +59,12 @@ primitives =
   , ("list?", unaryOp listTest)
   , ("symbol->string", unaryOp symbol2string)
   , ("string->symbol", unaryOp string2symbol)
+  , ("car", car)
+  , ("cdr", cdr)
+  , ("cons", cons)
+  , ("eq?", eqv)
+  , ("eqv?", eqv)
+  , ("equal?", equal)
   ]
 
 boolBinop ::
@@ -131,9 +139,6 @@ unpackBool :: LispVal -> ThrowsError Bool
 unpackBool (Bool b) = return b
 unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
 
---
--- List primitives
---
 car :: [LispVal] -> ThrowsError LispVal
 car [List (x:xs)] = return x
 car [DottedList (x:xs) _] = return x
@@ -170,3 +175,29 @@ eqv [(List arg1), (List arg2)] =
         Right (Bool val) -> val
 eqv [_, _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
+
+data Unpacker =
+  forall a. Eq a =>
+            AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do unpacked1 <- unpacker arg1
+     unpacked2 <- unpacker arg2
+     return $ unpacked1 == unpacked2
+     `catchError` (const $ return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <-
+    liftM or $
+    mapM
+      (unpackEquals arg1 arg2)
+      [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1, arg2]
+  return $
+    Bool $
+    (primitiveEquals ||
+     let (Bool x) = eqvEquals
+     in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
