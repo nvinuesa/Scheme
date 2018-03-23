e4caa9c8e16fd68ea8ed97f2443cbcbcf87fa7f8
underscorenico <vinuesa.nico@gmail.com>
2018-03-23 23:16:06 +0100

    Finished repl

--- src/Scheme/Evaluator.hs
+++ src/Scheme/Evaluator.hs
@@ -1,11 +1,40 @@
 {-# LANGUAGE ExistentialQuantification #-}
 
 module Scheme.Evaluator
-  ( eval
+  ( runRepl
+  , evalAndPrint
   ) where
 
 import Control.Monad.Except
 import Scheme.Data
+import Scheme.Parser
+import System.IO
+
+--
+-- REPL
+--
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
